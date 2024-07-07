506152d7f9df28d8dda66d97c7d5216aa1f1ca1e
LongYinan <lynweklm@gmail.com>
2024-07-07 21:27:20 +0800

    CARGO_INCREMENTAL

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,7 @@ const NODE_MODULES_PATH: &str = "/node_modules/";
 const NODE_MODULES_PATH: &str = "\\node_modules\\";
 
 #[cfg(not(target_os = "windows"))]
-const PATH_PREFIX: &str = "file://";
+const PATH_PREFIX: &str = "file:///";
 
 #[cfg(target_os = "windows")]
 const PATH_PREFIX: &str = "file://";
