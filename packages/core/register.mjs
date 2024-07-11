3a4d211b2d70f09db2992e4a304518a86bec1195
LongYinan <lynweklm@gmail.com>
2024-07-11 21:33:18 +0800

    chore: use import.meta.url to replace fileToPathURL

--- packages/core/register.mjs
+++ packages/core/register.mjs
@@ -1,4 +1,3 @@
 import { register } from 'node:module'
-import { pathToFileURL } from 'node:url'
 
-register('@oxc-node/core/esm', pathToFileURL('./').toString())
+register('@oxc-node/core/esm', import.meta.url)
