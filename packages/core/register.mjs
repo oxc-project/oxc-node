79e9245319278bc578df1be4979ab1c73384c3e6
LongYinan <lynweklm@gmail.com>
2024-07-11 19:03:23 +0800

    fix: import path

--- packages/core/register.mjs
+++ packages/core/register.mjs
@@ -1,4 +1,4 @@
 import { register } from 'node:module'
 import { pathToFileURL } from 'node:url'
 
-register('@oxc/node-core/esm', pathToFileURL('./').toString())
+register('@oxc-node/core/esm', pathToFileURL('./').toString())
