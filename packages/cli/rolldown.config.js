8740e05a97c33b99042824b09c92390421c90c81
LongYinan <lynweklm@gmail.com>
2024-07-23 15:01:45 +0800

    feat(cli): init oxnode (#23)

---
+++ packages/cli/rolldown.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'rolldown'
+
+export default defineConfig({
+  input: './src/index.ts',
+  resolve: {
+    conditionNames: ['module', 'node'],
+    mainFields: ['module', 'main'],
+  },
+  platform: 'node',
+  external: ['@oxc-node/core'],
+  treeshake: true,
+  output: {
+    format: 'esm',
+    assetFileNames: '[name].js',
+  },
+})
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,83 @@
+import process from 'node:process'
+import { exec, execSync } from 'node:child_process'
+
+import { Command, Option, Cli, Usage, Builtins } from 'clipanion'
+
+import pkgJson from '../package.json' with { type: 'json' }
+
+const [node, app, ...stdArgs] = process.argv
+
+class MainCommand extends Command {
+  static paths = [[]]
+
+  static usage: Usage = {
+    description: `Run a script with oxc transformer and oxc-resolver`,
+    details: `oxnode is a CLI tool that runs a script with oxc transformer and oxc-resolver.
+    The esm module is resolved by oxc-resolver and transformed by oxc transformer.
+    The cjs module support will be added in the future.
+    `,
+    examples: [
+      [`Run a script`, `oxnode ./src/index.ts`],
+      [`repl`, `oxnode`],
+    ],
+  }
+
+  help = Option.Boolean(`-h,--help`, false, {
+    description: `Show help`,
+  })
+
+  nodeHelp = Option.Boolean(`--node-help`, false, {
+    description: `Show Node.js help`,
+  })
+
+  args = Option.Rest()
+
+  async execute() {
+    if (this.help) {
+      this.context.stdout.write(this.cli.usage())
+      return
+    }
+    if (this.nodeHelp) {
+      this.args.push(`--help `)
+    }
+    const args = this.args.length ? ` ${this.args.join(' ')}` : ''
+    if (!args.length) {
+      execSync(`node --import @oxc-node/core/register`, {
+        env: process.env,
+        cwd: process.cwd(),
+        stdio: `inherit`,
+      })
+      return
+    }
+    const cp = exec(`node --import @oxc-node/core/register ${args}`, {
+      env: process.env,
+      cwd: process.cwd(),
+    })
+    cp.addListener(`error`, (error) => {
+      console.error(error)
+    })
+    if (cp.stdin) {
+      this.context.stdin.pipe(cp.stdin)
+    }
+    if (cp.stdout) {
+      cp.stdout.pipe(this.context.stdout)
+    }
+    if (cp.stderr) {
+      cp.stderr.pipe(this.context.stderr)
+    }
+    cp.addListener(`exit`, (code) => {
+      process.exit(code ?? 0)
+    })
+  }
+}
+
+const cli = new Cli({
+  binaryLabel: `oxnode`,
+  binaryName: `${node} ${app}`,
+  binaryVersion: pkgJson.version,
+})
+
+cli.register(MainCommand)
+cli.register(Builtins.HelpCommand)
+cli.register(Builtins.VersionCommand)
+cli.runExit(stdArgs)
