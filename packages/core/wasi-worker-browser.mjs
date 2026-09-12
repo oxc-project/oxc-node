41e68b29fc00dceb6b7b21a5bad75961f5ac3325
LongYinan <lynweklm@gmail.com>
2026-09-12 11:14:01 +0800

    chore: re-generate the binding files

--- packages/core/wasi-worker-browser.mjs
+++ packages/core/wasi-worker-browser.mjs
@@ -4,20 +4,20 @@ import {
   WASI,
   emnapiAsyncWorkPlugin,
   emnapiTSFNPlugin,
-} from "@napi-rs/wasm-runtime";
+} from '@napi-rs/wasm-runtime'
 
 const handler = new MessageHandler({
   onLoad({ wasmModule, wasmMemory }) {
     const wasi = new WASI({
       print: function () {
         // eslint-disable-next-line no-console
-        console.log.apply(console, arguments);
+        console.log.apply(console, arguments)
       },
       printErr: function () {
         // eslint-disable-next-line no-console
-        console.error.apply(console, arguments);
+        console.error.apply(console, arguments)
       },
-    });
+    })
     return instantiateNapiModuleSync(wasmModule, {
       childThread: true,
       wasi,
@@ -32,12 +32,12 @@ const handler = new MessageHandler({
           ...importObject.napi,
           ...importObject.emnapi,
           memory: wasmMemory,
-        };
+        }
       },
-    });
+    })
   },
-});
+})
 
 globalThis.onmessage = function (e) {
-  handler.handle(e);
-};
+  handler.handle(e)
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite-plus";
 
+const ignorePatterns = [
+  "**/fixtures/**",
+  "/packages/core/browser.js",
+  "/packages/core/index.js",
+  "/packages/core/index.d.ts",
+  "/packages/core/oxc-node.wasi.cjs",
+  "/packages/core/oxc-node.wasi-browser.js",
+  "/packages/core/wasi-worker-browser.mjs",
+  "/packages/core/wasi-worker.mjs",
+];
+
 export default defineConfig({
-  fmt: {},
+  fmt: {
+    ignorePatterns,
+  },
   staged: {
     "*": ["vp fmt --no-error-on-unmatched-pattern"],
     "*.@(js|ts|tsx)": ["vp lint --fix"],
@@ -11,6 +24,6 @@ export default defineConfig({
       typeAware: true,
       typeCheck: true,
     },
-    ignorePatterns: ["**/fixtures/**"],
+    ignorePatterns,
   },
 });
